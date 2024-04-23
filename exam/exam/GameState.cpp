ba971365cd0f00deaff8aabb5513302344767d99
XimaLc <74795630+XimaLc@users.noreply.github.com>
2024-04-23 13:09:44 +0200

    Update GameState.cpp

--- exam/exam/GameState.cpp
+++ exam/exam/GameState.cpp
@@ -1,12 +1,12 @@
 #include "GameState.h"
 #include "StateManager.h"
 
-Player GameState::m_player;
-sf::Texture GameState::playerTexture;
 bool GameState::isPlayerInit;
 
 GameState::GameState()
 {
+	m_player = new Player();
+
 	gameHasFocus = true;
 
 	timer = 0.f;
@@ -15,16 +15,15 @@ GameState::GameState()
 
 }
 
-Player GameState::getPlayer()
+GameState::GameState(Player& _player)
 {
-	if (!isPlayerInit)
-	{
-		playerTexture.loadFromFile("../Files/Textures/player.png");
-		m_player = Player("player", playerTexture);
-		isPlayerInit = true;
-	}
-		
-	return m_player; 
+	m_player = &_player;
+
+	gameHasFocus = true;
+
+	timer = 0.f;
+	map.setLayersTexture();
+	map.load("map");
 }
 
 void GameState::Update(sf::Vector2f _mousePos)
@@ -32,7 +31,7 @@ void GameState::Update(sf::Vector2f _mousePos)
 	if (hasMove)
 	{
 		hasMove = false;
-		if (map.isInGrass(m_player.getPosition()))
+		if (map.isInGrass(m_player->getPosition()))
 		{
 			
 			int x = iRand(0, 100);
@@ -47,7 +46,7 @@ void GameState::Update(sf::Vector2f _mousePos)
 void GameState::Draw(sf::RenderWindow& _window)
 {
 	map.Draw(_window, {1, 2, 3, 4, 5});
-	m_player.Draw(_window);
+	m_player->Draw(_window);
 	m_inGameMenu.Draw(_window);
 }
 
@@ -63,44 +62,44 @@ void GameState::HandleKeyboard(sf::Event _event)
 	{
 		if (_event.key.code == sf::Keyboard::D && timer > 0.1f)
 		{
-			if (m_player.getPosition().x < map.getWidth())
-				if (map.canGo(m_player.getPosition(), { 1, 0 }))
+			if (m_player->getPosition().x < map.getWidth())
+				if (map.canGo(m_player->getPosition(), { 1, 0 }))
 				{
-					m_player.move(1, 0);
-					m_player.anim(1, 0);
+					m_player->move(1, 0);
+					m_player->anim(1, 0);
 					hasMove = true;
 				}
 			timer = 0.f;
 		}
 		if (_event.key.code == sf::Keyboard::Q && timer > 0.1f)
 		{
-			if (m_player.getPosition().x > 0)
-				if (map.canGo(m_player.getPosition(), { -1, 0 }))
+			if (m_player->getPosition().x > 0)
+				if (map.canGo(m_player->getPosition(), { -1, 0 }))
 				{
-					m_player.move(-1, 0);
-					m_player.anim(-1, 0);
+					m_player->move(-1, 0);
+					m_player->anim(-1, 0);
 					hasMove = true;
 				}
 			timer = 0.f;
 		}
 		if (_event.key.code == sf::Keyboard::S && timer > 0.1f)
 		{
-			if (m_player.getPosition().y < map.getHeight())
-				if (map.canGo(m_player.getPosition(), { 0, 1 }))
+			if (m_player->getPosition().y < map.getHeight())
+				if (map.canGo(m_player->getPosition(), { 0, 1 }))
 				{
-					m_player.move(0, 1);
-					m_player.anim(0, 1);
+					m_player->move(0, 1);
+					m_player->anim(0, 1);
 					hasMove = true;
 				}
 			timer = 0.f;
 		}
 		if (_event.key.code == sf::Keyboard::Z && timer > 0.1f)
 		{
-			if (m_player.getPosition().y > 0)
-				if (map.canGo(m_player.getPosition(), { 0, -1 }))
+			if (m_player->getPosition().y > 0)
+				if (map.canGo(m_player->getPosition(), { 0, -1 }))
 				{
-					m_player.move(0, -1);
-					m_player.anim(0, -1);
+					m_player->move(0, -1);
+					m_player->anim(0, -1);
 					hasMove = true;
 				}
 			timer = 0.f;
