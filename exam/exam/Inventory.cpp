c2294b7db99d9f55073130e2713c5b63d891d08b
XimaLc <74795630+XimaLc@users.noreply.github.com>
2024-04-26 09:26:55 +0200

    t

--- exam/exam/Inventory.cpp
+++ exam/exam/Inventory.cpp
@@ -5,3 +5,8 @@ Inventory::Inventory()
 {
 	
 }
+
+int Inventory::getBallAmount(int _id)
+{
+	return m_ballInventory.getBallAmount(_id);
+}
--- exam/exam/Inventory.h
+++ exam/exam/Inventory.h
@@ -6,5 +6,8 @@ class Inventory
 	BallInventory m_ballInventory;
 public:
 	Inventory();
+
+	int getBallAmount(int _id);
+		
 };
 
